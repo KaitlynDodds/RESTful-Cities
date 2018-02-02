9c1b615bad577a30d189161fb19381c8924e8b7a
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-02 07:57:49 -0700

    setting up dependencies

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express'),
+      mongoose = require('mongoose'),
+      bodyParser = require('body-parser'),
+      methodOverride = require('method-override'),
+      app = express();
