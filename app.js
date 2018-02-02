158534562d0b1e5843ff22c0bddcfe55d33b90c6
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-02-02 08:05:32 -0700

    express server configured

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const express = require('express'),
-      mongoose = require('mongoose'),
+const methodOverride = require('method-override'),
       bodyParser = require('body-parser'),
-      methodOverride = require('method-override'),
+      mongoose = require('mongoose'),
+      express = require('express'),
       app = express();
+
+// APP SET UP
+mongoose.connect('mongodb://127.0.0.1/cities');     // Mongoose connection
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));                 // Enable PUT and DELETE
+
+// INDEX
+app.get('/', function(req, res) {
+  res.send('Hello, world!');
+});
+
+// server
+app.listen(3000, () => console.log('App listening on port 3000'));
